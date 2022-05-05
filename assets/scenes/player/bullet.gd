0ae6ac73ce1d63fdcc3e48011c20b10cc1570470
ikenton <imkenton@cpp.edu>
2022-05-05 15:04:52 -0700

    Deleted un-used AnimatedSprite.gd

--- assets/scenes/player/bullet.gd
+++ assets/scenes/player/bullet.gd
@@ -9,6 +9,4 @@ var direction = Vector2.RIGHT
 func _physics_process(delta):
 	#moves bullet right
 	position.x += speed
-	#moves bullet left
-	#position.x -= speed
-	#TODO addchild position node instead of physically placing it?
+	
